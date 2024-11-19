fa370578efa582a74f0eb87eb552408db2addc5b
sasi1278 <sasi.chimmili@gmail.com>
2024-11-19 10:15:54 +0530

    adding safe area view

--- App.tsx
+++ App.tsx
@@ -1,41 +1,17 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import CustomText from './src/components/CustomText';
+import { SafeAreaView, ScrollView, View,} from 'react-native';
+import CounterRedux from './src/screens/ConterRedux';
+import Sample from './src/screens/Sample';
 
 function App() {
   return (
     <SafeAreaView style={{flex:1}}>
       <View style={{flex:1}}>
-        <CustomText size={10}>
-          Hi
-        </CustomText>
+        <CounterRedux/>
+        <Sample/>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
